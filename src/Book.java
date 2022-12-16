3cbc8ec17fe5d01bd1d79a38b2f9568603a94e8e
BerilOtcu <110414969+BerilOtcu@users.noreply.github.com>
2022-12-16 19:09:01 +0000

    Trying to solve

---
+++ src/Book.java
@@ -0,0 +1,24 @@
+public class Book {
+
+    //Harry Potter
+    //hunger games
+
+        public String title,type, author;
+        public int price;
+
+        public void setInfo(String title, String type, String author, int price){
+            this.title =title;
+            this.type = type;
+            this.author = author;
+            this.price = price;
+        }
+
+
+        public String toString () {
+            return""
+
+}
+
+
+
+}
