98e4bd819e6da1df07024062d1f5bf9381abf42a
amroziatabssam <amroziaerol@yahoo.co.uk>
2022-12-16 19:54:38 +0000

    both questions are answered

--- src/Amrozia_ConceptQuestions.java
+++ src/Amrozia_ConceptQuestions.java
@@ -3,7 +3,15 @@ public class Amrozia_ConceptQuestions {
 /*
 
 - What is the Instance method?
+-It is a method that belongs to "Object" rather than class.
+-It is Declared without "static" keyword (non-static method).
+-In order to call instance methods, we need to create an "Object".
+
 - Can we access instance variables by using the static method?
+-No Because it is a instance variable which belongs to an object and it cannot be static.
+
+-
+-
 
 
  */
