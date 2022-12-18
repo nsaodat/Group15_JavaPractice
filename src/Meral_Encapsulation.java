ec3347b74a4c5313ee33e152e0a42d8b62abcb52
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2022-12-18 23:39:47 +0000

    Hello :)
    Please check your respective Task and respond.
    
    Many thanks,
    Nafisa

---
+++ src/Meral_Encapsulation.java
@@ -0,0 +1,22 @@
+public class Meral_Encapsulation {
+    /*
+    create a class named BankAccount
+        Private variables:
+            accountHolder, accountNumber, balance
+
+        encapsulate all the fields
+
+        Add a constructor that allows user to set all the fields when the object is created.
+                        (If the arguments not valid it should not be set to the instances)
+
+        Extra methods:
+                deposit()
+                withdraw()
+                checkbalance()
+                toString()
+
+            DO NOT duplicate any code fragments
+
+            HINT --> check day35
+     */
+}
