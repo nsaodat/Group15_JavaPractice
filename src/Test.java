68f855c3f2561d5d72da5463e5945ea84eabc546
Nafisa Ravshanova <nafisa.ravshanova@outlook.com>
2022-12-21 01:29:37 +0000

    Hello :)
    Please check your respective Task and respond.
    
    Many thanks,
    Nafisa

--- src/Test.java
+++ src/Test.java
@@ -6,7 +6,7 @@ public class Test {
         Item item = new Item("Apple", 2.3, 20);
 
         System.out.println( item.calcCost()); // 46.0
-        System.out.println(item); // Item{name='Apple', unitPrice=2.3, quantity= $20, total price= $20}
+        System.out.println(item); // Item{name='Apple', unitPrice=2.3, quantity= 20, total price= $46.0}}
 
         item.setName("Melon");
         item.setUnitPrice(5);
